5add2b939757c1654ab344b9a472ee015a235bfc
TaeyoungYoo <yty7320@gmail.com>
2022-09-11 17:59:20 +0900

    rank search

---
+++ 2021_kako_blind/rank_search.py
@@ -0,0 +1,11 @@
+import collections
+from itertools import combinations
+info = ["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150",
+        "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"]
+query = ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200",
+         "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100",
+         "- and - and - and - 150"]
+
+for i in range(5):
+    comb = list(combinations(range(4), i))
+    print(comb)
