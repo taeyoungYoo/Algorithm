d7529dc81d9fb9b2f6f9dabb3b7a9b6a7d61463e
taeyoungYoo <52447154+taeyoungYoo@users.noreply.github.com>
2022-09-16 01:01:23 +0900

    kakao 2021 blind 합승 택시 - floyd warshall approach

--- 2021_kako_blind/carpooling_taxi.py
+++ 2021_kako_blind/carpooling_taxi.py
@@ -1,16 +1,34 @@
-def solution(n, s, a, b, fares):
-    answer = INF = 987654321
-    adj = [[INF] * (n+1) for _ in range(n+1)]
-    for i in range(n+1):
-        adj[i][i] = 0
+'''
+카카오 2021 Blind Recruitment
+합승 택시 요금
+
+Floyd-warshall approach : O(V^3)
+'''
+
+
+# convert fares into 2-dim array, graph
+def create_graph(n, fares):
+    INF = 987654321
+    graph = [[INF] * (n + 1) for _ in range(n + 1)]
+    for i in range(n + 1):
+        graph[i][i] = 0
     for x, y, z in fares:
-        adj[x][y] = z
-        adj[y][x] = z
+        graph[x][y] = z
+        graph[y][x] = z
+    return graph
+
+
+def solution(n, s, a, b, fares):
+    answer = 987654321
+    # graph initiation
+    graph = create_graph(n, fares)
+    # floyd-warshall
     for k in range(1, n+1):
         for i in range(1, n+1):
             for j in range(1, n+1):
-                if adj[i][k] + adj[k][j] < adj[i][j]:
-                    adj[i][j] = adj[i][k] + adj[k][j]
+                if graph[i][k] + graph[k][j] < graph[i][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
+    # find node i that minimize the cost to node a and b
     for i in range(1, n+1):
-        answer = min(answer, adj[s][i] + adj[i][a] + adj[i][b])
+        answer = min(answer, graph[s][i] + graph[i][a] + graph[i][b])
     return answer
