7965bb21d3145be87236d82da9c16e90b15277fb
taeyoungYoo <yty7320@gmail.com>
2022-10-05 19:28:22 +0900

    프로그래머스 소수 찾기 eratosthenes, permutation approach

--- Brute force/taeyoung_searching_prime.py
+++ Brute force/taeyoung_searching_prime.py
@@ -22,7 +22,7 @@ def solution(numbers):
     # 만들 수 있는 max value를 구해 그 이하의 소수 구하기
     worst = list(numbers)
     worst.sort(reverse=True)
-    eratostenes(int(''.join(worst)))
+    eratosthenes(int(''.join(worst)))
     numbers = list(numbers)
     # 만들 수 있는 경우의 수 소수인치 판별
     for i in range(1, len(numbers)+1):
